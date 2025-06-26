499229fda46a5ff6612725e044b2faaa5da01df9
soniji-Tanishque33 <157713836+soniji-Tanishque33@users.noreply.github.com>
2025-06-26 12:18:46 +0530

    Time: 0 ms (100%), Space: 18.7 MB (6.03%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>> ans;
-        ans = image;
-        int n = image.size();
-        int m = image[0].size();
-        queue<pair<int, int>> q;
-        int initial = image[sr][sc];
-        q.push({sr, sc});
-        // top right bottom left
-        int dx[] = {0, +1, 0, -1};
-        int dy[] = {+1, 0, -1, 0};
-
-        while(!q.empty()) {
-            auto p = q.front();
-            q.pop();
-            int row = p.first;
-            int col = p.second;
-            ans[row][col] = color; 
+    void dfs(int row, int col, vector<vector<int>> image, vector<vector<int>>& ans, int color, int initial,
+        int dx[], int dy[]) {
+            int n = image.size();
+            int m = image[0].size();
+            ans[row][col] = color;
             for(int i = 0; i<4; i++) {
-                int nrow = row - dx[i];
-                int ncol = col - dy[i];
+                int nrow = row + dx[i];
+                int ncol = col + dy[i];
                 if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == initial && ans[nrow][ncol] != color) {
-                    q.push({nrow, ncol});
+
+                    dfs(nrow, ncol, image, ans, color, initial, dx, dy);
                 }
             }
 
-        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<int>> ans = image;
+        int initial = image[sr][sc];
+        int dx[] = {0, +1, 0, -1};
+        int dy[] = {+1, 0, -1, 0};
+        dfs(sr, sc, image, ans, color, initial, dx, dy);
 
         return ans;
     }
