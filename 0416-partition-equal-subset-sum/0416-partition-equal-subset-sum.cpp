480e8374c63048ad472d1c9d9915b48a8ebf7093
soniji-Tanishque33 <157713836+soniji-Tanishque33@users.noreply.github.com>
2025-07-08 12:14:44 +0530

    Time: 87 ms (77.73%), Space: 13.5 MB (87.32%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -33,27 +33,29 @@ public:
         if(total % 2 != 0) return false;
 
         int target = total/2;
-        vector<vector<bool>> dp(n, vector<bool>(target+1, false));
-        for(int i = 0; i<n; i++) dp[i][0] = true;
-        if(nums[0] <= target) dp[0][nums[0]] = true;
+        vector<bool> curr(target+1, false), prev(target+1, false);
+        // for(int i = 0; i<n; i++) dp[i][0] = true;
+        // if(nums[0] <= target) dp[0][nums[0]] = true;
 
         for(int i = 1; i<n; i++) {
+            prev[0] = true;
             for(int j = 0; j<=target; j++) {
                  // take
                 int take = 0;
                 if(j >= nums[i]) {
-                    take = dp[i-1][j-nums[i]];
+                    take = prev[j-nums[i]];
                 }
 
                 // not take
-                int nottake = dp[i-1][j];
+                int nottake = prev[j];
 
-                dp[i][j] = take || nottake;
+                curr[j] = take || nottake;
             }
+            prev = curr;
         }
 
 
 
-        return dp[n-1][target];
+        return prev[target];
     }
 };
