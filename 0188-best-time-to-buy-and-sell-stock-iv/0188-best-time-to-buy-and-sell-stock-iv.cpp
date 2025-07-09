e9923ee90dae6bbcfa4e94f54a745cd4dacf6a19
soniji-Tanishque33 <157713836+soniji-Tanishque33@users.noreply.github.com>
2025-07-10 01:01:55 +0530

    Time: 8 ms (53.59%), Space: 18.2 MB (46.38%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -30,7 +30,31 @@ public:
     }
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k, -1)));
-        return f(0, k, true, 0, prices, dp);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+
+        for(int index = n-1; index >= 0; index--) {
+            for(int canBuy = 0; canBuy <= 1; canBuy++) {
+                for(int transactions = k-1; transactions >=0; transactions--) {
+                    // buying
+                    int profit = 0;
+                    if(canBuy) {
+                        profit = max(
+                            -prices[index] + dp[index+1][0][transactions],
+                            dp[index+1][1][transactions]
+                        );
+                    } else {
+                        // selling
+                        profit = max(
+                            prices[index] + dp[index+1][1][transactions+1],
+                            dp[index+1][0][transactions]
+                        );
+                    }
+
+                    dp[index][canBuy][transactions] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][0];
     }
 };
