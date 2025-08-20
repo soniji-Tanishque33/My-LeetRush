2b5d39632b55358b37849ad967712c1079c51e74
soniji-Tanishque33 <157713836+soniji-Tanishque33@users.noreply.github.com>
2025-08-20 23:46:58 +0530

    Time: 474 ms (11.74%), Space: 223.9 MB (24.78%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    int f(int idx, int transaction, int canBuy, vector<int>& prices, vector<vector<vector<int>>>& dp) {
-        if(idx == prices.size()) {
-            return 0;
-        }
-        if(transaction >= 2) return 0;
-        if(dp[idx][canBuy][transaction] != -1) return dp[idx][canBuy][transaction];
+    using vvvi = vector<vector<vector<int>>>;
+    int f(int index, bool buy, int k, vector<int>& prices, vvvi& dp) {
+        // base case
+        if(k == 0) return 0;
+        if(index == prices.size()) return 0;
+        if(dp[index][buy][k] != -1) return dp[index][buy][k];
 
-        int profit = 0;
-        if(canBuy) {
-            // buying
-            // taking the max of take and not take
+        // recurrsion
+        int profit = INT_MIN;
+        if(buy) {
             profit = max(
-                -prices[idx] + f(idx+1, transaction, 0, prices, dp),
-                f(idx+1, transaction, 1, prices, dp)
+                -prices[index] + f(index+1, 0, k, prices, dp),
+                0 + f(index+1, 1, k, prices, dp)
             );
         } else {
-            // selling
             profit = max(
-                prices[idx] + f(idx+1, transaction+1, 1, prices, dp),
-                f(idx+1, transaction, 0, prices, dp)
+                +prices[index] + f(index+1, 1, k-1, prices, dp),
+                0+ f(index+1, 0, k, prices, dp)
             );
         }
 
-        return dp[idx][canBuy][transaction] = profit;
-
+        return dp[index][buy][k] = profit;
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return f(0,0, 1, prices, dp);
-        
+        return f(0, 1, 2, prices, dp);
     }
 };
