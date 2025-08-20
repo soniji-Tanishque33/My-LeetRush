74814ad694c5a4ff00d5c7724629aa17172b5f24
soniji-Tanishque33 <157713836+soniji-Tanishque33@users.noreply.github.com>
2025-08-21 01:49:19 +0530

    Time: 11 ms (53.33%), Space: 95.8 MB (59.51%) - LeetHub

--- 1622-max-value-of-equation/1622-max-value-of-equation.cpp
+++ 1622-max-value-of-equation/1622-max-value-of-equation.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, int>> pq; // {yi-xi, xi}
-        int ans = INT_MIN;
-        for(int i = 0; i<points.size(); i++) {
-            while(!pq.empty() && points[i][0] - pq.top().second > k) {
-                pq.pop();
-            }
-            if(!pq.empty()) {
-                ans = max(ans, points[i][0] + points[i][1] + pq.top().first);
-            }
+       deque<pair<int, int>> dq;
+       int ans = INT_MIN;
 
-            pq.push({points[i][1] - points[i][0], points[i][0]});
-        }
+       for(int i = 0; i<points.size(); i++) {
+            while(!dq.empty() && points[i][0] - dq.front().second > k) {
+                dq.pop_front();
+            }
+            if(!dq.empty()) {
+                ans = max(ans, points[i][0] + points[i][1] + dq.front().first);
+            }
+            while(!dq.empty() && points[i][1] - points[i][0] > dq.back().first) {
+                dq.pop_back();
+            }
+            dq.push_back({points[i][1] - points[i][0], points[i][0]});
+       }
 
-        return ans;
+       return ans;
     }
 };
