e8ea3acbddd00f975af5bb279ab5dfe4a9e6d5d3
soniji-Tanishque33 <157713836+soniji-Tanishque33@users.noreply.github.com>
2025-08-23 11:52:43 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.93%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int f(int i, int j, vector<vector<int>>& dp) {
+        /// base case
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        // recurrsion
+        return dp[i][j] = f(i-1, j, dp) + f(i, j-1, dp);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return f(m-1, n-1, dp);
+    }
+};
