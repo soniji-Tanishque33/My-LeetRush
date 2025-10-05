b590063a832908f164cde0e503656672d9e02a5c
soniji-Tanishque33 <157713836+soniji-Tanishque33@users.noreply.github.com>
2025-10-05 19:56:04 +0530

    Time: 7 ms (3.86%), Space: 12.5 MB (66.12%) - LeetHub

---
+++ 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> mp;
+        int i = 0;
+        for(char ch : order) {
+            mp[ch] = 'a' + i;
+            i++;
+        }
+
+        for(string& w : words) {
+            for(char& ch : w) {
+                ch = mp[ch];
+            }
+            cout << w << endl;
+        }
+
+        return is_sorted(words.begin(), words.end());
+    }
+};
