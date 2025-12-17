740dee48b397a9afeb11f04e8fb8bae4d32a4a89
soniji-Tanishque33 <157713836+soniji-Tanishque33@users.noreply.github.com>
2025-12-17 22:35:37 +0530

    Time: 36 ms (90.13%), Space: 40 MB (87.78%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -1,63 +1,58 @@
 class Solution {
 public:
     priority_queue<int> left; // maxheap
-    priority_queue<int, vector<int>, greater<>> right; // minHeap;
-    unordered_map<int, int> lazy_delete; // index : count;
+    priority_queue<int, vector<int>, greater<>> right; // minheap
+    unordered_map<int, int> lazy_delete;
     int left_size = 0;
     int right_size = 0;
 
-
-    void prune_left(priority_queue<int>& left) {
-        while(!left.empty() && lazy_delete[left.top()] > 0) {
+    void prune(priority_queue<int>& left) {
+        while(left.size() > 0 && lazy_delete[left.top()]) {
             lazy_delete[left.top()]--;
             left.pop();
         }
     }
 
-    void prune_right(priority_queue<int, vector<int>, greater<>>& right) {
-        while(!right.empty() && lazy_delete[right.top()] > 0) {
+    void prune(priority_queue<int, vector<int>, greater<>>& right) {
+        while(right.size() > 0 && lazy_delete[right.top()]) {
             lazy_delete[right.top()]--;
             right.pop();
         }
     }
-
-
     
 
-    double get_median() {
-        // always prune before
-        prune_left(left);
-        prune_right(right);
-
-        if(left_size != right_size) {
-            return (double)left.top();
-        }
-        double ans = ((double)left.top() + (double)right.top()) / 2.0;
-        return ans;
-    }
-
     void balance() {
-        if(left_size > right_size + 1) {
-            right.push(left.top());
-            left.pop();
-            right_size++;
-            left_size--;
-        }
-        if(right_size > left_size) {
+        if(right_size >  left_size) {
             left.push(right.top());
             right.pop();
             left_size++;
             right_size--;
         }
-
-        prune_left(left);
-        prune_right(right);
+        if(left_size > right_size+1) {
+            right.push(left.top());
+            left.pop();
+            right_size++;
+            left_size--;
+        }
+        prune(left);
+        prune(right);
     }
 
-    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        
-        vector<double> res;
+    double get_median() {
+        // DON'T FORGET TO PRUNE BEFORE TAKING ANSWER
+        // prune(left);
+        // prune(right);
 
+        if(left_size != right_size) {
+            // we will return the top of the left maxheap
+            return (double) left.top();
+        }
+        return ((double) right.top() + (double) left.top()) / 2.0;
+    }
+
+
+
+    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
         for(int i = 0; i<k; i++) {
             left.push(nums[i]);
             left_size++;
@@ -66,40 +61,43 @@ public:
         for(int i = 0; i<k/2; i++) {
             right.push(left.top());
             left.pop();
-            right_size++;
             left_size--;
+            right_size++;
         }
 
-        double ans = get_median();
-        res.push_back(ans);
+        vector<double> res;
 
+        res.push_back(get_median());
 
         for(int i = k; i<nums.size(); i++) {
-            int out_num = nums[i-k];
             int in_num = nums[i];
+            int out_num = nums[i-k];
 
-            if(left_size > 0 && out_num <= left.top()) {
-                lazy_delete[out_num]++;
+            // removing num from the heaps
+            lazy_delete[out_num]++;
+            if(out_num <= left.top()) {
+                // it will definately be in the left side
                 left_size--;
             } else {
-                lazy_delete[out_num]++;
+                // then it will be on the right
                 right_size--;
             }
 
             balance();
 
-            if(left.empty() || in_num < left.top()) {
-                left.push(in_num); left_size++;
-            }
-            else {
+            // inserting the new number
+            if(in_num <= left.top()) {
+                left.push(in_num);
+                left_size++;
+            } else {
                 right.push(in_num);
                 right_size++;
             }
 
             balance();
 
-
             res.push_back(get_median());
+
 
         }
 
@@ -107,6 +105,6 @@ public:
 
         return res;
 
-        
+
     }
 };
