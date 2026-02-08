01ce4e0cc720aee2adee6042bc9b33d94b53947a
soniji-Tanishque33 <157713836+soniji-Tanishque33@users.noreply.github.com>
2026-02-08 23:28:53 +0530

    Time: 0 ms (100%), Space: 23 MB (82.16%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,17 +11,18 @@
  */
 class Solution {
 public:
-    int max_diff= INT_MIN;
+    int maxdiff = INT_MIN;
     int f(TreeNode* root) {
         if(root == NULL) return 0;
-        int lh = f(root->left);
-        int rh = f(root->right);
-        max_diff = max(max_diff, abs(lh - rh));
-        // cout << root->val << ' ' << lh << ' ' << rh << ' ' << max_diff << endl;
-        return 1+max(lh, rh);
+        int lh = 1+f(root->left);
+        int rh = 1+f(root->right);
+        maxdiff =  max(maxdiff, abs(rh-lh));
+
+        return max(lh, rh);
     }
     bool isBalanced(TreeNode* root) {
+        if(root==NULL) return true;
         f(root);
-        return max_diff <= 1;
+        return maxdiff <= 1;
     }
 };
