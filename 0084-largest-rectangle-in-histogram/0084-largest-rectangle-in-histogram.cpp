ed3023cda84fbbc5c47c22f7c07f8459bfe93f52
soniji-Tanishque33 <157713836+soniji-Tanishque33@users.noreply.github.com>
2026-09-13 22:48:12 +0530

    Time: 23 ms (54.15%), Space: 85.7 MB (33.62%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,47 +2,37 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> prev_smaller;
         
+        vector<int> next_smaller(n, n);
+        vector<int> prev_smaller(n, -1);
+
         stack<int> st;
 
-        for(int i = 0; i<heights.size(); i++) {
-            while(st.size() && heights[st.top()] >= heights[i]) {
-                st.pop();
-            }
-            if(st.size() == 0) prev_smaller.push_back(-1);
-            else {
-                prev_smaller.push_back(st.top());
-            }
+        for(int i = 0; i<n; i++) {
+            while(st.size() > 0 && heights[st.top()] >= heights[i]) st.pop();
+
+            if(st.size() != 0) prev_smaller[i] = st.top();
 
             st.push(i);
         }
 
 
         st = {};
-        vector<int> next_smaller(n, 0);
 
-        for(int i = heights.size()-1; i>=0; i--) {
-            while(st.size() && heights[st.top()] >= heights[i]) {
-                st.pop();
-            }
-            if(st.size() ==0) next_smaller[i] = n;
-            else {
-                next_smaller[i] = st.top();
-            }
+        for(int i = n-1; i>=0 ; i--) {
+            while(st.size() > 0 && heights[st.top()] >= heights[i]) st.pop();
+
+            if(st.size() != 0) next_smaller[i] = st.top();
 
             st.push(i);
         }
 
+
         int ans = 0;
+
         for(int i = 0; i<n; i++) {
-            ans = max(ans, (next_smaller[i] - prev_smaller[i]-1) * heights[i]);
+            ans = max(ans, heights[i] * (next_smaller[i] - prev_smaller[i] - 1));
         }
-
         return ans;
-
-
-
-
     }
 };
